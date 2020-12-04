0f130f2d9b020379e22518565e76e0ebae088b23
Alex Borisov <isip_a.a.borisow@mpt.ru>
2020-12-04 17:44:55 +0300

    Initial commit

---
+++ app/src/main/java/ru/konder/myapplicationsql/MainActivity.kt
@@ -0,0 +1,73 @@
+package ru.konder.myapplicationsql
+
+import android.database.Cursor
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var peopleDB: DatabaseHelper? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnAdd = findViewById<Button>(R.id.btnAdd)
+        val btnShow = findViewById<Button>(R.id.btnShow)
+        peopleDB = DatabaseHelper(this@MainActivity)
+        btnAdd.setOnClickListener {
+            addUser()
+        }
+        btnShow.setOnClickListener{
+            showUsers()
+        }
+        btnDelete.setOnClickListener{
+            if(edtID!=null)
+                peopleDB!!.deleteData(edtID.text.toString())
+        }
+    }
+    private fun addUser() {
+        val edtName = findViewById<EditText>(R.id.edtName)
+        val edtEmail = findViewById<EditText>(R.id.edtEmail)
+        val name: String = edtName.text.toString()
+        val email: String = edtEmail.text.toString()
+        val insertData: Boolean = peopleDB!!.addData(name, email)
+        if (insertData) {
+            Toast.makeText(this@MainActivity, "Запись добавлена!", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this@MainActivity, "Что-то пошло не так!" , Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun showUsers(){
+        val data: Cursor? = peopleDB!!.showData()
+        if(data!!.count == 0 ){
+            display("Error", "Нет данных.")
+            return
+        }
+        val buffer = StringBuffer()
+        while (data.moveToNext()){
+            buffer.append(
+                    "ID:" + data.getString(0) + "\n"
+            )
+            buffer.append(
+                    "Name:" +data.getString(1) + "\n"
+            )
+            buffer.append(
+                    "Email:" +data.getString(2) + "\n"
+            )
+            display("Все пользователи", buffer.toString())
+        }
+    }
+    private fun deleteUsers(){
+
+    }
+    fun display (title: String?, message: String?){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setCancelable(true)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.show()
+    }
+}
